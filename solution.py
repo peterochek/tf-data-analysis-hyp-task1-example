ae91dc0a2c44df684e1a946a13172c23ccacbd3f
Peter Korolev <peterkkor@gmail.com>
2024-05-04 17:09:16 +0300

    fix error

--- solution.py
+++ solution.py
@@ -9,10 +9,11 @@ def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
     x_rate = x_success / x_cnt
     y_rate = y_success / y_cnt
 
-    z_score = (y_rate - x_rate) / (
-        (x_rate * (1 - x_rate) / x_cnt) + (y_rate * (1 - y_rate) / y_cnt)
-    ) ** 0.5
+    se_x = (x_rate * (1 - x_rate) / x_cnt) ** 0.5
+    se_y = (y_rate * (1 - y_rate) / y_cnt) ** 0.5
 
-    critical_z = 1.96
+    z_score = (y_rate - x_rate) / ((se_x**2 + se_y**2) ** 0.5)
 
-    return abs(z_score) > critical_z
+    critical_z = 1.4051
+
+    return z_score > critical_z
